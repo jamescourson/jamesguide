2f6391aef84b3067ab3b74b01df4d4a0dec80e1f
jamescourson <brokencontigo@gmail.com>
2018-12-04 00:04:15 -0500

    cors/middleware fixes

--- app.js
+++ app.js
@@ -5,22 +5,32 @@ var connection = require('./config/db.js');
 // Initialize express
 var app = express();
 
+// Define port
+const port = 3000;
+
+// Enable CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // Enable body parsing
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 
 // Define api route
 app.get('/api/:id', (req, res) => {
-  connection.connect();
-
-  connection.query(`SELECT * FROM ${req.query.id}`, (err, result) => {
+  connection.query(`SELECT * FROM ${req.params.id}`, (err, result) => {
     res.json(result);
   });
-
-  connection.end();
 });
 
+app.get('/', (req, res) => {
+  res.send('Welcome to the James\' Guide API!');
+})
+
 // Start server
-app.listen(80, () => {
-  console.log('Listening on port 80');
+app.listen(port, () => {
+  console.log('Listening on port %s', port);
 });
